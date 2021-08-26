911d6df343967a4040c597525a0e12278569f338
BuildTools <ldrj.leoleo@gmail.com>
2021-08-26 16:15:26 -0300

    Project setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:fragged_sheets/pages/pages.dart';
 
 void main() {
-  runApp(MyApp());
+  // Initialize stuff here.
+
+  runApp(
+    FraggedSheets()
+  );
 }
 
-class MyApp extends StatelessWidget {
+class FraggedSheets extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Fragged Sheets',
+      theme: ThemeData.dark(),
+      home: MyHomePage(title: 'Fragged Sheets'),
+      supportedLocales: [
+        const Locale('en', ''), // English, no country code
+        const Locale('pt', ''), // Portuguese, no country code
+        const Locale('es', ''), // Spanish, no country code
+      ],
     );
   }
 }
